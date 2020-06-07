c310b29b7ac89b1b37fe02cd9591d4f63ce35260
caiiiookk <caiiiookk@gmail.com>
2020-06-07 10:38:32 +0000

    Updated routing.py

---
+++ EES/routing.py
@@ -0,0 +1,19 @@
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from channels.auth import AuthMiddlewareStack
+from django.urls import path
+
+from chat_app.consumers import ChatConsumer
+
+application = ProtocolTypeRouter({
+    # Empty for now (http->django views is added by default)
+    'websocket': AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(
+                [
+                    path('ws/chat/<str:chat_type>/<int:chat_id>/', ChatConsumer, name='chatting'),
+                ]
+            )
+        )
+    )
+})
